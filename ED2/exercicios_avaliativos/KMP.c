fb801cd3a8b2e31829da16400997b049783d4a72
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2024-11-08 15:49:46 -0300

    Add files via upload

---
+++ ED2/exercicios_avaliativos/KMP.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ConstruirPrefixos(const char *chave, int m, int *aux);
+int KMP(const char* frase, char* chave);
+
+int main(){
+    char chave[20];
+    const char* frase = "O rato roeu a roupa do rei de roma";
+
+    printf("Digite a chave a ser buscada: ");
+    scanf("%s", chave);
+
+    int resultado = KMP(frase, chave);
+
+    if(resultado == -1){
+        printf("Chave nao encontrada.\n");
+    }else{
+        printf("Chave encontrada apos %d comparacoes.\n", resultado);
+    }
+    return 0;
+}
+void ConstruirPrefixos(char *chave, int m, int *aux){
+    int j = -1;
+    aux[0] = j;
+
+    for(int i = 1; i < m; i++){
+        while(j > -1 && chave[j + 1] != chave[i]){
+            j = aux[j];
+        }
+        if(chave[i] == chave[j + 1]){
+            j++;
+        }
+        aux[i] = j;
+    }
+}
+
+int KMP(const char* frase, char* chave){
+    int n = strlen(frase);
+    int m = strlen(chave);
+    int aux[m];
+    int cont = 0;
+
+    ConstruirPrefixos(chave, m, aux);
+    cont++;
+    int j = -1;
+
+    for(int i = 0; i < n; i++){
+        cont++;
+        
+        while(j > -1 && chave[j + 1] != frase[i]){
+            j = aux[j];
+            cont++;
+        }
+         if(chave[j + 1] == frase[i]) { j++; }
+        if(j == m - 1){
+            return cont;
+        }
+    }
+    return -1;
+}
